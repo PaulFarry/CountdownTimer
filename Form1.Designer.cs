53c2906f79d2c922d13b9794c598ba5c489c5e46
Paul Farry <paul.farry@qsl.com.au>
2019-10-29 13:58:19 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CountDownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrCountdown = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrCountdown
+            // 
+            this.tmrCountdown.Tick += new System.EventHandler(this.TmrCountdown_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(661, 114);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "10d99:99:99";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(661, 114);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Countdown";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCountdown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownTimer
+{
+    public partial class Form1 : Form
+    {
+
+        private DateTime starttime;
+        private DateTime endTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            starttime = new DateTime(2019, 10, 24, 10, 10, 0);
+            endTime = new DateTime(2019, 11, 4);
+
+            this.StartPosition = FormStartPosition.Manual;
+            tmrCountdown.Enabled = true;
+        }
+
+        private void TmrCountdown_Tick(object sender, EventArgs e)
+        {
+            var difference = endTime - DateTime.Now;
+            label1.Text = $"{difference.Days:00}d{difference.Hours:00}:{difference.Minutes:00}:{difference.Seconds:00}";
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            var wa = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+            var newLocation = new Point( wa.Right - this.Width, wa.Bottom - this.Height);
+            this.Location = newLocation;
+        }
+    }
+}
