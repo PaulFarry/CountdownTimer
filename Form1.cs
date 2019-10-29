21f95a9d39165683616ecf89f4cf085a12b52cdd
PaulFarry <paulfarry@gmail.com>
2019-10-29 21:00:11 +1000

    Corrected EndTime :-(

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CountDownTimer
@@ -22,7 +15,7 @@ namespace CountDownTimer
             InitializeComponent();
 
             starttime = new DateTime(2019, 10, 24, 10, 10, 0);
-            endTime = new DateTime(2019, 11, 4);
+            endTime = new DateTime(2019, 11, 4, 6, 0, 0);
 
             this.StartPosition = FormStartPosition.Manual;
             tmrCountdown.Enabled = true;
@@ -37,7 +30,7 @@ namespace CountDownTimer
         private void Form1_Shown(object sender, EventArgs e)
         {
             var wa = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
-            var newLocation = new Point( wa.Right - this.Width, wa.Bottom - this.Height);
+            var newLocation = new Point(wa.Right - this.Width, wa.Bottom - this.Height);
             this.Location = newLocation;
         }
     }
